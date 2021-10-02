df84211df738e0a4d63c0f650db7c41d32727108
Cody Horst <chorst22@gmail.com>
2021-10-01 23:05:58 -0700

    adding initial map with hitboxes

---
+++ assets/map/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Background" tilewidth="240" tileheight="160" tilecount="1" columns="1">
+ <image source="background.png" width="240" height="160"/>
+</tileset>
