50db50191683ab7a393d824fc18b9342d81e9ed0
Curtis Thompson <curtisrthompson@outlook.com>
2021-10-02 00:05:10 -0700

    Started main.lua

---
+++ main.lua
@@ -0,0 +1,33 @@
+-- Full Medical Alchemist
+
+-- In the criminal justice system, sexually based offenses are considered especially heinous.
+-- In New York City, the dedicated detectives who investigate these vicious felonies are members 
+-- of an elite squad known as the Special Victims Unit. 
+-- These are their stories.
+
+-- Load Modules / Libraries
+-- local json = require"3rd/json/json"
+
+-- Declare Global Parameters Here
+-- WINDOW_WIDTH = 1280
+-- WINDOW_HEIGHT = 720
+
+-- Define Local Parameters Here
+local sti = require"3rd/sti/sti"
+local player = require"player"
+
+-- A primary callback of LÖVE that is called only once
+function love.load()
+
+end
+
+-- A primary callback of LÖVE that is called continuously
+function love.update(dt)
+
+end
+
+-- A primary callback of LÖVE that is called continuously
+function love.draw()
+
+end
+
