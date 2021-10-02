7d785ef2380e721a1cbf1cd4957b3b3dcd00b17c
Rusty Roberts <rust.roberts@protonmail.com>
2021-10-02 12:49:28 -0700

    testing sti in main

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@
 -- These are their stories.
 
 -- Load Modules / Libraries
+-- local background = require"background"
+local sti = require"3rd/sti/sti"
 local physicker = require"physicker"
 local frog = require"frog"
 
@@ -14,21 +16,28 @@ WindowWidth = love.graphics.getWidth()
 WindowHeight = love.graphics.getHeight()
 
 -- Define Local Parameters Here
+local map = sti("assets/map/map.lua", {"box2d"})
 
 -- A primary callback of LÖVE that is called only once
 function love.load()
+    World = love.physics.newWorld(0, 0)
+    love.physics.setMeter(32)
+    map:box2d_init(World)
     physicker:load()
     frog:load()
 end
 
 -- A primary callback of LÖVE that is called continuously
 function love.update(dt)
+    map:update(dt)
     physicker:update(dt)
     frog:update(dt)
 end
 
 -- A primary callback of LÖVE that is called continuously
 function love.draw()
+    map:drawTileLayer("Background")
+    map:box2d_draw()
     physicker:draw()
     frog:draw()
 end
