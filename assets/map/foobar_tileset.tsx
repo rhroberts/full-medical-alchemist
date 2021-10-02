9b965b7dedf8acf214c72ed8667224a978946cb8
Cody Horst <chorst22@gmail.com>
2021-10-02 13:27:26 -0700

    transition map to true tileset. add WIP alchemy screen

---
+++ assets/map/foobar_tileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="foobar" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="8" height="8" source="tiles/floor.png"/>
+ </tile>
+ <tile id="4">
+  <image width="4" height="4" source="tiles/wall.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="tiles/workbench.png"/>
+ </tile>
+</tileset>
