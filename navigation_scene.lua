c8ffc0be829d2c56edb088ee9a8c98ec044bf45f
Marcus Bakke <marcusabakke@gmail.com>
2021-10-03 22:22:29 -0700

    randomize and load patients. They will walk to their beds

--- navigation_scene.lua
+++ navigation_scene.lua
@@ -3,6 +3,8 @@ Scene = require "scene"
 local sti = require "3rd/sti/sti"
 local physicker = require"physicker"
 local frog = require"frog"
+local cat = require"cat"
+local patient = require"patient"
 local textbox = require"textbox"
 
 local navigation_scene = Scene:new("navigation")
@@ -17,6 +19,22 @@ function navigation_scene:load()
 
     physicker:load()
     frog:load()
+    cat:load()
+    -- Randomize patients
+    local P = {1, 2, 3, 4}
+    for i = 1, #P - 1 do
+        local j = math.random(i, #P)
+        P[i], P[j] = P[j], P[i]
+    end
+    p1 = patient:new(P[1], 1, 0)
+    p2 = patient:new(P[2], 2, 3)
+    p3 = patient:new(P[3], 3, 6)
+    p4 = patient:new(P[4], 4, 9)
+    p1:load()
+    p2:load()
+    p3:load()
+    p4:load()
+
     -- add an example text box
     Greeting = textbox(
         "Move around, bro! Work the room. Explore this beautiful world."
@@ -25,11 +43,17 @@ function navigation_scene:load()
     -- tunez
     NavTheme = love.audio.newSource("assets/audio/music/navigation_scene.ogg", "static")
 end
+  
 
 function navigation_scene:update(dt, gamestate)
     World:update(dt)
     physicker:update(dt)
     frog:update(dt)
+    cat:update(dt)
+    p1:update(dt)
+    p2:update(dt)
+    p3:update(dt)
+    p4:update(dt)
     if love.keyboard.isDown("e") then
         gamestate:setAlchemyScene()
     end
@@ -45,6 +69,11 @@ function navigation_scene:draw(sx, sy)
     Map:draw(0, 0, sx, sy)
     physicker:draw()
     frog:draw()
+    cat:draw()
+    p1:draw()
+    p2:draw()
+    p3:draw()
+    p4:draw()
     love.graphics.pop()
     Greeting.draw()
 end
@@ -52,11 +81,13 @@ end
 function BeginContact(a, b, collision)
     physicker:beginContact(a, b, collision)
     frog:beginContact(a, b, collision)
+    cat:beginContact(a, b, collision)
 end
 
 function EndContact(a, b, collision)
     physicker:endContact(a, b, collision)
     frog:endContact(a, b, collision)
+    cat:endContact(a, b, collision)
 end
 
 return navigation_scene
